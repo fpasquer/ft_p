ae024303e9108badd868e6c75464bfa907130bb7
fpasquer <fpasquer@student.42.fr>
2017-10-28 18:06:03 +0200

    initial commit start2

--- server/incs/server.h
+++ server/incs/server.h
@@ -6,12 +6,12 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/28 17:27:29 by fpasquer          #+#    #+#             */
-/*   Updated: 2017/10/28 17:52:36 by fpasquer         ###   ########.fr       */
+/*   Updated: 2017/10/28 18:05:17 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_P_SERVER_H
-# define FT_P_SERVER_H
+#ifndef SERVER_H
+# define SERVER_H
 
 # include "../../libft/libft.h"
 # include <stdio.h>
