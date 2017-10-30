a4475ba882ed4d86406f34b2563cfe119156c289
Florian PASQUEREAU <fpasquer@e1r5p12.42.fr>
2017-10-30 11:31:07 +0100

    new way to show errors term

--- general_srcs/term.c
+++ general_srcs/term.c
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/29 12:43:08 by fpasquer          #+#    #+#             */
-/*   Updated: 2017/10/30 10:59:20 by fpasquer         ###   ########.fr       */
+/*   Updated: 2017/10/30 11:29:54 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,8 @@ t_cmd_manager				g_cmds[] = {
 	{NULL, NULL}
 };
 
-static bool					print_prompt_cmd(char const cmd[SIZE_CMD])
+static bool					print_prompt_cmd(char const cmd[SIZE_CMD],
+		int const wrong_cmd)
 {
 	t_gen					*gen;
 
@@ -36,28 +37,15 @@ static bool					print_prompt_cmd(char const cmd[SIZE_CMD])
 			cmd == NULL)
 		return (false);
 	wclear(gen->scr.term) ;
+	if (wrong_cmd == true)
+		mvwprintw(gen->scr.term, 0, 0, "ft_p: command not found", gen->cmd);
 	mvwprintw(gen->scr.term, 1 , 1, "%s%s", PROMPT, cmd);
 	wrefresh(gen->scr.term);
 	return (true);
 }
 
-static void					print_wrong_cmd(t_gen *gen, size_t const len_cmd)
-{
-	int						y;
-	clock_t					start;
-
-	if (gen == NULL)
-		return ;
-	y = (len_cmd + LEN_PROMPT)/ COLS;
-	mvwprintw(gen->scr.term, 2 + y, 1, "ft_p: command not found",
-			gen->cmd);
-	wrefresh(gen->scr.term);
-	start = clock();
-	while (clock() <= start + WAIT_TIME)
-		;
-}
-
-int							get_cmd(char cmd[SIZE_CMD], size_t *i)
+int							get_cmd(char cmd[SIZE_CMD], size_t *i,
+		int const wrong_cmd)
 {
 	ssize_t					len;
 	char					key[SIZE_BUFF];
@@ -67,7 +55,7 @@ int							get_cmd(char cmd[SIZE_CMD], size_t *i)
 		return (-1);
 	ft_bzero(cmd, sizeof(char) * SIZE_CMD);
 	(*i) = 0;
-	while ((*i) < SIZE_CMD - 1 && print_prompt_cmd(cmd) == true)
+	while ((*i) < SIZE_CMD - 1 && print_prompt_cmd(cmd, wrong_cmd) == true)
 	{
 		if ((len = get_key_pressed(key)) < 0 ||
 				ft_strncmp(key, KEY_ENTER_, SIZE_BUFF) == 0)
@@ -93,17 +81,16 @@ int							loop_term(t_gen *gen)
 
 	if (gen == NULL || gen->scr.term == NULL)
 		return (-1);
+	wrong_cmd = false;
 	while (1)
 	{
-		wrong_cmd = true;
-		if (get_cmd(gen->cmd, &len_cmd) != 0)
+		if (get_cmd(gen->cmd, &len_cmd, wrong_cmd) != 0)
 			del_general(EXIT_FAILURE);
 		i = 0;
+		wrong_cmd = true;
 		while (g_cmds[i].cmd != NULL)
 			if (ft_strcmp(g_cmds[i++].cmd, gen->cmd) == 0)
 				wrong_cmd = g_cmds[i - 1].f();
-		if (wrong_cmd == true)
-			print_wrong_cmd(gen, len_cmd);
 	}
 	return (0);
 }
