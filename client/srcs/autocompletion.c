d14ccfe5b7b2260bbd26d987f1286b834b0f00da
fpasquer <fpasquer@student.42.fr>
2017-11-21 08:17:11 +0100

    autocompletion bin ok 2

--- client/srcs/autocompletion.c
+++ client/srcs/autocompletion.c
@@ -6,14 +6,11 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/19 08:52:07 by fpasquer          #+#    #+#             */
-/*   Updated: 2017/11/21 08:08:49 by fpasquer         ###   ########.fr       */
+/*   Updated: 2017/11/21 08:16:24 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/client.h"
-
-static char const 			*g_list_cmd[] = {"CD", "GET", "LOGIN", "LOGOUT",
- 		"LS", "PUT", "PWD", "QUIT", NULL};
 
 static int					nb_match_in_list(char **list, char const *needed)
 {
@@ -77,41 +74,6 @@ static int					autocompletion_exe(t_gen *gen, char const
 	return (ret);
 }
 
-// static int					autocompletion_bin(t_gen *gen, char const *cmd)
-// {
-// 	char					**list;
-// 	int						ret;
-// 	int						nb_match;
-// 	unsigned int			i;
-// 	size_t					len_needed;
-
-// 	if (gen == NULL)
-// 		return (-1);
-// 	if ((nb_match = nb_match_in_list((char**)g_list_cmd, cmd)) <= 0)
-// 		return (nb_match);
-// 	list = NULL;
-// 	i = 0;
-// 	len_needed = ft_strlen(cmd);
-// 	while (g_list_cmd[i] != NULL)
-// 		if (ft_strncmp(g_list_cmd[i++], cmd, len_needed) == 0)
-// 		{
-// 			if (nb_match == 1)
-// 				return (autocomplet_word(gen, g_list_cmd[i - 1]));
-// 			if ((list = ft_add_to_array((char *)g_list_cmd[i - 1], list)) ==
-// 					NULL)
-// 				break ;
-// 		}
-// 	ret = save_ret_server(gen->i_client.list_cmd, list);
-// 	ft_free_add_to_array(list);
-// 	return (ret);
-// }
-
-// static int					autocompletion_content(t_gen *gen, char const *cmd)
-// {
-// 	if (gen == NULL)
-// 		return (-1);
-// }
-
 static int					check_autocompletion_bin(char const *cmd)
 {
 	unsigned int			i;
@@ -128,6 +90,8 @@ static int					check_autocompletion_bin(char const *cmd)
 
 int							autocompletion(void)
 {
+	char const 				*list_cmd[] = {"CD", "GET", "LOGIN", "LOGOUT",
+ 		"LS", "PUT", "PWD", "QUIT", NULL};
 	char					*cmd;
 	int						ret;
 	t_gen					*gen;
@@ -135,17 +99,15 @@ int							autocompletion(void)
 	if ((gen = get_general(NULL)) == NULL || add_cmd_list(
 			&gen->i_client.list_cmd, gen->cmd, NULL) != 0)
 		return (-1);
-	// if (gen->len_cmd == 0)
-	// 	return (save_ret_server(gen->i_client.list_cmd, (char**)g_list_cmd));
 	cmd = gen->cmd + gen->len_cmd - 1;
 	while (cmd > gen->cmd && ft_isspace(*(cmd - 1)) == false)
 		cmd--;
 	if ((ret = check_autocompletion_bin(gen->cmd)) < 0)
 		return (-1);
 	if (ret == true)
-		return (autocompletion_exe(gen, (char const **)g_list_cmd, cmd));
+		return (autocompletion_exe(gen, list_cmd, cmd));
 	if (gen->win == SERVER)
-		return (autocompletion_exe(gen, (char const **)gen->cwd_server.list, cmd));
+		return (autocompletion_exe(gen, (char const **)gen->cwd_server.list,
+				cmd));
 	return (autocompletion_exe(gen, (char const **)gen->cwd_client.list, cmd));
-	// return (ret == true ? autocompletion_bin(gen, cmd) : 0); //ajouter la fonction du contenu du dossier
 }
